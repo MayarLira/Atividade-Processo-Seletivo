6a2cccee03972a5f12725033a65b2692fb4436cc
Mayara <mayara.lira092@gmail.com>
2024-03-26 18:30:08 -0300

    "Exercício 1 com a resposta no comentário"

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+
+using namespace std;
+
+int main() {
+int indice = 13, soma = 0, k = 0;
+
+while(k<indice)
+{
+    k = k + 1;
+    soma = soma + k;
+}
+
+cout << soma;
+// Portanto, a variavel soma tem o valor de  91.
+
+return 0;
+
+}
